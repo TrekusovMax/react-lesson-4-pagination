d997f1ec7867bbb76e955728209b50c4d9519846
Трекусов Максим Владимирович <trekusov_mv@interrao.ru>
2021-12-14 11:21:40 +0300

    developed pagination

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,37 +1,58 @@
-import React from 'react'
-import User from './user'
+import React, { useState } from "react"
+import { paginate } from "../ulits/paginate"
+import Pagination from "./pagination"
+import User from "./user"
 
 const Users = ({ users, ...rest }) => {
+  const count = users.length
+  const pageSize = 4
+  const [currentPage, setCurrentPage] = useState(1)
+  const handlePageChange = (pageIndex) => {
+    setCurrentPage(pageIndex)
+  }
+
+  const userCrop = paginate(users, currentPage, pageSize)
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Имя</th>
-          <th scope="col">Качества</th>
-          <th scope="col">Профессия</th>
-          <th scope="col">Встретился, раз</th>
-          <th scope="col">Оценка</th>
-          <th scope="col">Избранное</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user) => (
-          <User
-            key={user._id}
-            qualities={user.qualities}
-            _id={user._id}
-            name={user.name}
-            profession={user.profession}
-            completedMeetings={user.completedMeetings}
-            rate={user.rate}
-            handleDelete={rest.handleDelete}
-            handleToggleBookMark={rest.handleToggleBookMark}
-            favorite={user.favorite}
-          />
-        ))}
-      </tbody>
-    </table>
+    <>
+      {count > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col">Избранное</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {userCrop.map((user) => (
+              <User
+                key={user._id}
+                qualities={user.qualities}
+                _id={user._id}
+                name={user.name}
+                profession={user.profession}
+                completedMeetings={user.completedMeetings}
+                rate={user.rate}
+                handleDelete={rest.handleDelete}
+                handleToggleBookMark={rest.handleToggleBookMark}
+                favorite={user.favorite}
+              />
+            ))}
+          </tbody>
+        </table>
+      )}
+      <Pagination
+        itemsCount={count}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </>
   )
 }
 
